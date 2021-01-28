6da04176c7f74ef1ed8e29f389e616952181c302
Natchakot Chuchokjatuporn <hacker092322@gmail.com>
2021-01-28 19:47:41 +0700

    Send Assignment Week1

---
+++ GI455_Project/Assets/Text Matching/Scripts/FindWords.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FindWords : MonoBehaviour
+{
+    public Text inputWord, answerText;
+
+    string checkWord;
+    bool isCorrect = false;
+
+    public void FindButton()
+    {
+        isCorrect = false;
+        checkWord = inputWord.GetComponent<Text>().text;
+
+        for (int i = 0; i < SetupWords.words.Count; i++)
+        {
+            if (checkWord == SetupWords.words[i])
+            {
+                isCorrect = true;
+                break;
+            }
+        }
+
+        if (isCorrect == true)
+        {
+            answerText.text = "[ <color=green>" + checkWord + "</color> ] is found.";
+        }
+
+        else
+        {
+            answerText.text = "[ <color=red>" + checkWord + "</color> ] is not found.";
+        }
+    }
+}
---
+++ GI455_Project/Assets/Text Matching/Scripts/SetupWords.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetupWords : MonoBehaviour
+{
+    public static List<string> words = new List<string>();
+
+    string wordInTag;
+
+    void Start()
+    {
+        
+        foreach (GameObject textTag in GameObject.FindGameObjectsWithTag("SearchWords"))
+        {
+            wordInTag = textTag.GetComponent<Text>().text;
+            words.Add(wordInTag);
+        }
+
+        //Test Add List and print
+        //for (int i = 0; i < words.Count; i++)
+        //{
+        //    print(words[i]);
+        //}
+        
+    }
+}
